72d7066339c9756666de4cf102c0546a33a7cdde
Princu7 <princu7@gmail.com>
2019-02-04 00:31:21 +0530

    removed console statements

--- public/client.js
+++ public/client.js
@@ -1,10 +1,10 @@
-// const socket = io.connect('https://rocketium-editor-abhishek.herokuapp.com/' || 'http://localhost:5000');
-const socket = io.connect('http://localhost:5000');
+// const socket = io.connect('https://rocketium-editor-abhishek.herokuapp.com/'); // For Heroku
+const socket = io.connect('http://localhost:5000'); // For Localhost
 var editRequestSent = false;
 var users = [];
-var deltasArr = [];
-var curEditing = false;
-var curEditingUser;
+var deltasArr = []; // List of all the text changes. Stored as Quill editor special objects deltas
+var curEditing = false; // Boolean storing whether this user is currently editing or not
+var editAccessUser; // Stores the socket ID of the user with the edit access
 
 const options = {
   placeholder: 'Write what you want. Write what you will',
@@ -14,20 +14,26 @@ const options = {
 const quill = new Quill('#editor', options);
 
 
-// First init message containing all the text entered till now from the server
-socket.on('init', initHandler);
-socket.on('newEditor', checkAccessHandler);
-socket.on('userConnect', userAddHandler);
-socket.on('userDisconnect', userLeftHandler);
-socket.on('newTextUpdate', textUpdateHandler);
-quill.on('text-change', textChangeHandler);
-quill.on('selection-change', selectionChangeHandler);
+socket.on('init', initHandler); // Init message from server, contains initialization information
+socket.on('newEditor', checkAccessHandler); // Message from server informing about the new user who got edit access
+socket.on('userConnect', userAddHandler); // New user connection message from server
+socket.on('userDisconnect', userLeftHandler); // User disconnection message from server
+socket.on('newTextUpdate', textUpdateHandler); // Message from server containing text changes made by the editing client
+quill.on('text-change', textChangeHandler); // Event fired by Quill editor when changes are made locally
+quill.on('selection-change', selectionChangeHandler); // Event fired by Quill when the cursor focus is changed
 
+
+/*
+Handles the addition of user to the existing array
+*/
 function userAddHandler(data) {
   users.push(data.id);
   updatePeerDisplay();
 }
 
+/*
+Handles the disconnection of an existing user
+*/
 function userLeftHandler(data) {
   if (users.indexOf(data.id) >= 0) {
     users.splice(users.indexOf(data.id), 1);
@@ -35,49 +41,55 @@ function userLeftHandler(data) {
   }
 }
 
+/*
+Asks the server for edit access of the document
+*/
 function getEditAccess() {
   if (editRequestSent) {
     return;
   }
-  console.log('Sending get edit access');
   socket.emit('getEditAccess', socket.id);
-  editRequestSent = true;
+  editRequestSent = true; // Boolean for avoiding repeated messages to the server asking for edit access
 }
 
+/*
+Ugly function for displaying current active user and present user
+*/
 function updatePeerDisplay() {
   $('#active').html('');
   $('#present').html('');
 
-  console.log('Inside peer display');
-  console.log('users', users);
-  console.log('Cur user', curEditingUser);
-  console.log('Cur editing', curEditing);
-
   if (curEditing) {
     $('#active').html('<p>You</p>')
-  } else if (curEditingUser) {
-    $('#active').html(`<p>User: ${curEditingUser} </p>`)
+  } else if (editAccessUser) {
+    $('#active').html(`<p>User: ${editAccessUser} </p>`)
   }
   users.forEach((user) => {
-    if (user == curEditingUser) {
+    if (user == editAccessUser) {
       return;
     }
     if (user == socket.id) {
-      $('#present').append('<p> You </p>')
+      if (editRequestSent) {
+        $('#present').append('<p>You (Edit Request Sent)</p>')
+      } else {
+        $('#present').append('<p>You</p>')
+      }
     } else {
-      $('#present').append(`<p> User: ${user} </p>`)
+      $('#present').append(`<p>User ID: ${user}</p>`)
     }
   });
 }
 
+/*
+Initializes the editor and data variables with information received from server. And requests for edit access
+*/
 function initHandler(data) {
-  console.log('Inside init handler');
   users = data.socketIds;
   deltasArr = data.deltas;
-  curEditingUser = data.curEditSocket;
-  console.log('data', data);
-  updatePeerDisplay();
+  editAccessUser = data.curEditSocket;
+
   getEditAccess();
+  updatePeerDisplay();
 
   if (deltasArr.length == 0) {
     return;
@@ -85,15 +97,17 @@ function initHandler(data) {
   deltasArr.forEach((delta) => {
     quill.updateContents(delta);
   });
+
   quill.update('api');
-  console.log('Updated all');
 }
 
+/*
+Receives current editing user from server and matches it against the present user
+*/
 function checkAccessHandler(socketId) {
-  console.log('Check access handler ');
-  curEditingUser = socketId;
+  editAccessUser = socketId;
+
   if (socket.id == socketId) {
-    console.log('Currente client now editing');
     quill.enable(true);
     quill.focus();
     editRequestSent = false;
@@ -101,35 +115,44 @@ function checkAccessHandler(socketId) {
     updatePeerDisplay();
     return;
   }
+
   updatePeerDisplay();
   quill.disable();
 }
 
+/*
+Updates edtior with latest changes received from the server
+*/
 function textUpdateHandler(data) {
-  console.log('Text update handler from server');
   const delta = data.delta;
+
   deltasArr.push(delta);
   quill.updateContents(delta);
   quill.update('api');
 }
 
+/*
+Notifies the server about latest text changes made locally in the editor
+*/
 function textChangeHandler(delta, oldDelta, source) {
-  console.log('Text change handler to server');
-  if (source == 'api') {
+  if (source == 'api') { // Don't emit the text update event when the change made is through the API itself
     return;
   }
+
   deltasArr.push(delta);
   socket.emit('textUpdate', {delta});
 }
 
+/*
+Sends request to server for gaining or leaving edit access based on change of focus of mouse cursor
+*/
 function selectionChangeHandler(range, oldRange, source) {
-  if (curEditing && !range) { // Cursor is not in the editor. Give the chance to other users to edit :)
-    console.log('Sent access change to server. Other users now can edit');
+  if (curEditing && !range) { // User clicked outside the editor. Wants to give up edit access :)
     socket.emit('accessChange');
     editRequestSent = false;
     curEditing = false;
-  } else if (range && !curEditing) {
-    console.log('Ask for edit access from server');
+  } else if (range && !curEditing) { // User clicked within the editor. Wants edit access
     getEditAccess();
+    updatePeerDisplay();
   }
 }
--- server.js
+++ server.js
@@ -7,82 +7,87 @@ const io = socket(server);
 server.listen(process.env.PORT || 5000);
 app.use(express.static('public'));
 
-console.log('Why no logs');
-
-const deltas = [];
-const socketIds = [];
-const waitingUsers = [];
-var curEditSocket;
+const deltas = []; // List of all the text changes made so far. Stored in form of Deltas (Quill editor objects)
+const socketIds = []; // List of all IDs of client sockets connected so far
+const waitingUsers = []; // Stores the list of clients who have requested for edit access
+var curEditSocket; //ID of the current editing socket client
 
 io.sockets.on('connection', connection);
 
 function connection(socket) {
-	console.log('what the fuck');
-	console.log('a new user with id ' + socket.id + " has entered");
 	socketIds.push(socket.id);
+
 	io.emit('userConnect', {id: socket.id});
+	io.to(socket.id).emit('init', {socketIds, deltas, curEditSocket}); // Sent the whole initial content to the connected user
 
-	// Sent the whole initial content to the connected user
-	io.to(socket.id).emit('init', {socketIds, deltas, curEditSocket});
+	socket.on('disconnect', disconnectHandler); // When a client disconnects
+	socket.on('getEditAccess', getAccessHandler); // When a client requests for edit access
+	socket.on('textUpdate', textUpdateHandler); // When a client notified of local text change
+	socket.on('accessChange', changeAccessHandler) // When a client gives up edit access
 
-	socket.on('disconnect', disconnectHandler);
-	socket.on('getEditAccess', getAccessHandler);
-	socket.on('textUpdate', textUpdateHandler);
-	socket.on('accessChange', changeAccessHandler)
-
+	/*
+	Handles the disconnection of a client
+	*/
 	function disconnectHandler() {
-		console.log(socket.id, ' disconnected');
-		socketIds.splice(socketIds.indexOf(socket.id), 1);
-		io.emit('userDisconnect', {id: socket.id});
+		const socketIndex = socketIds.indexOf(socket.id);
 
-		console.log('waiting users', waitingUsers);
+		if (socketIndex >= 0) {
+			socketIds.splice(socketIndex, 1);
+		}
 
-		if (curEditSocket !== socket.id) {
-			const index = waitingUsers.indexOf(socket.id);
-			if (index > -1) {
-				waitingUsers.splice(index, 1);
+		io.emit('userDisconnect', {id: socket.id}); //Broadcast disconnect event to all clients
+
+		if (curEditSocket !== socket.id) { // Disconnecting client wasn't the one with edit access
+			const sockIndex = waitingUsers.indexOf(socket.id); // Remove if present in waiting queue
+			if (sockIndex > -1) {
+				waitingUsers.splice(sockIndex, 1);
 			}
-		} else {
+		} else { // Disconnecting client was the one with the edit access
 			if (!waitingUsers.length) {
 				curEditSocket = null;
-				io.emit('newEditor', curEditSocket);
+				io.emit('newEditor', curEditSocket); // No one currently has access. Broadcasting null
 				return;
 			}
 			curEditSocket = waitingUsers.shift();
-			io.emit('newEditor', curEditSocket);
+			io.emit('newEditor', curEditSocket); // Broadcasting the socket id of the client with edit access now
 		}
 	}
 
+	/*
+	Handler when a client requests for edit accesss
+	*/
 	function getAccessHandler(socketId) {
-		console.log('Inside get acess handler');
-		console.log('Wiating users', waitingUsers);
-		if (curEditSocket && waitingUsers.indexOf(socketId) == -1) {
+		if (curEditSocket && waitingUsers.indexOf(socketId) == -1) { // Lock not free. If not in waiting queue already, push it
 			waitingUsers.push(socketId);
 			return;
 		}
 
-		if (!curEditSocket && waitingUsers.length == 0) {
+		if (!curEditSocket && waitingUsers.length == 0) { // No one currently editing and no one in queue. Grant access
 			curEditSocket = socketId;
 			io.emit('newEditor', socketId);
 		}
 	}
 
+	/*
+	Receives text changes from editing client and broadcasts it
+	*/
 	function textUpdateHandler(data) {
 		const newDelta = data.delta;
 		deltas.push(newDelta);
 		socket.broadcast.emit('newTextUpdate', {delta: newDelta});
 	}
 
+	/*
+	Gives the editing access to the other clients after the current one decides to give it up
+	*/
 	function changeAccessHandler() {
-		console.log('Inside change access handler');
-		console.log('Waiting uers', waitingUsers);
 		if (!waitingUsers.length) {
 			curEditSocket = null;
 			io.emit('newEditor', curEditSocket);
 			return;
 		}
 
-		curEditSocket = waitingUsers.shift();
+		curEditSocket = waitingUsers.shift(); // Give access in FIFO or queue order
 		io.emit('newEditor', curEditSocket);
 	}
 }
