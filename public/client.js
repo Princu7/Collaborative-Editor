6e0db13680aca08d82d3dbfacf2f8ff94992aebe
Princu7 <princu7@gmail.com>
2019-02-03 18:05:09 +0530

    Initial commit. Working okayish. With debug statements

---
+++ public/client.js
@@ -0,0 +1,99 @@
+const socket = io.connect('http://localhost:8080');
+const toolbarOptions = ['bold', 'italic', 'underline', 'strike'];
+var editRequestSent = false;
+var users = [];
+var deltasArr = [];
+var curEditing = false;
+
+const options = {
+  // debug: 'info',
+  placeholder: 'Compose an epic...',
+  readOnly: true,
+  theme: 'snow'
+};
+const quill = new Quill('#editor', options);
+
+
+// First init message containing all the text entered till now from the server
+socket.on('init', initHandler);
+socket.on('newEditor', checkAccessHandler);
+socket.on('userConnect', userAddHandler);
+socket.on('userDisconnect', userLeftHandler);
+socket.on('newTextUpdate', textUpdateHandler);
+quill.on('text-change', textChangeHandler);
+quill.on('selection-change', selectionChangeHandler);
+
+function userAddHandler(data) {
+  users.push(data.id);
+}
+
+function userLeftHandler(data) {
+  users.splice(users.indexOf(data.id), 1);
+}
+
+function getEditAccess() {
+  if (editRequestSent) {
+    return;
+  }
+  console.log('Sending get edit access');
+  socket.emit('getEditAccess', socket.id);
+  editRequestSent = true;
+}
+
+function initHandler(data) {
+  console.log('Inside init handler');
+  users = data.socketIds;
+  deltasArr = data.deltas;
+  console.log('Deltas arr', deltasArr);
+  getEditAccess();
+
+  if (deltasArr.length == 0) {
+    return;
+  }
+  deltasArr.forEach((delta) => {
+    quill.updateContents(delta);
+  });
+  quill.update('api');
+  console.log('Updated all');
+}
+
+function checkAccessHandler(socketId) {
+  console.log('Check access handler ');
+  if (socket.id == socketId) {
+    console.log('Currente client now editing');
+    quill.enable(true);
+    editRequestSent = false;
+    curEditing = true;
+    return;
+  }
+  quill.disable();
+}
+
+function textUpdateHandler(data) {
+  console.log('Text update handler from server');
+  const delta = data.delta;
+  deltasArr.push(delta);
+  quill.updateContents(delta);
+  quill.update('api');
+}
+
+function textChangeHandler(delta, oldDelta, source) {
+  console.log('Text change handler to server');
+  if (source == 'api') {
+    return;
+  }
+  deltasArr.push(delta);
+  socket.emit('textUpdate', {delta});
+}
+
+function selectionChangeHandler(range, oldRange, source) {
+  if (curEditing && !range) { // Cursor is not in the editor. Give the chance to other users to edit :)
+    console.log('Sent access change to server. Other users now can edit');
+    socket.emit('accessChange');
+    editRequestSent = false;
+    curEditing = false;
+  } else if (range && !curEditing) {
+    console.log('Ask for edit access from server');
+    getEditAccess();
+  }
+}
---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
+const server = require('http').createServer(app);
+const io = socket(server);
+
+server.listen(8080);
+console.log('Server listening on port 8080');
+app.use(express.static('public'));
+
+const deltas = [];
+const socketIds = [];
+const waitingUsers = [];
+var curEditSocket;
+
+io.sockets.on('connection', connection);
+
+function connection(socket) {
+	console.log('a new user with id ' + socket.id + " has entered");
+	socketIds.push(socket.id);
+	io.emit('userConnect', {id: socket.id});
+
+	// Sent the whole initial content to the connected user
+	io.to(socket.id).emit('init', {socketIds, deltas});
+
+	socket.on('disconnect', disconnectHandler);
+	socket.on('getEditAccess', getAccessHandler);
+	socket.on('textUpdate', textUpdateHandler);
+	socket.on('accessChange', changeAccessHandler)
+
+	function disconnectHandler() {
+		console.log(socket.id, ' disconnected');
+		socketIds.splice(socketIds.indexOf(socket.id), 1);
+		io.emit('userDisconnect', {id: socket.id});
+
+		console.log('waiting users', waitingUsers);
+
+		if (curEditSocket !== socket.id) {
+			const index = waitingUsers.indexOf(socket.id);
+			if (index > -1) {
+				waitingUsers.splice(index, 1);
+			}
+		} else {
+			if (!waitingUsers.length) {
+				curEditSocket = null;
+				return;
+			}
+			curEditSocket = waitingUsers.shift();
+			io.emit('newEditor', curEditSocket);
+		}
+	}
+
+	function getAccessHandler(socketId) {
+		console.log('Inside get acess handler');
+		console.log('Wiating users', waitingUsers);
+		if (curEditSocket && waitingUsers.indexOf(socketId) == -1) {
+			waitingUsers.push(socketId);
+			return;
+		}
+
+		if (!curEditSocket && waitingUsers.length == 0) {
+			curEditSocket = socketId;
+			io.emit('newEditor', socketId);
+		}
+	}
+
+	function textUpdateHandler(data) {
+		const newDelta = data.delta;
+		deltas.push(newDelta);
+		socket.broadcast.emit('newTextUpdate', {delta: newDelta});
+	}
+
+	function changeAccessHandler() {
+		console.log('Inside change access handler');
+		console.log('Waiting uers', waitingUsers);
+		if (!waitingUsers.length) {
+			curEditSocket = null;
+			return;
+		}
+
+		curEditSocket = waitingUsers.shift();
+		io.emit('newEditor', curEditSocket);
+	}
+}
